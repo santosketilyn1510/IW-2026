6fd7178264a0801ab18035ae637b0f4e4bfe8e3a
ketilyn santos <santosketilyn1510@gmail.com>
2026-02-23 08:47:41 -0300

    Add files via upload

---
+++ AULA01/variaveis.js
@@ -0,0 +1,31 @@
+var nomeDaVariavel = "valor da variável";
+var outravariavel = "valor da outra variável";
+var variavelIdade = 22;
+var variavelTenhoCabeloBranco = false;
+var variavelIndefinida ;
+
+console.log(nomeDaVariavel);
+console.log(outravariavel);
+console.log(variavelIdade);
+console.log(variavelTenhoCabeloBranco);
+console.log(variavelIndefinida);
+
+
+var a = 14;
+var b = 42;
+var tr = (a == 14);
+var fl = (a == b);
+
+//neste caso tr irá conter o valor true e fl o valor false.
+
+var int1 = tr + 1;
+var int2 = fl + 1;
+
+//a variavel int1 irá conter o valor 2 (true + 1), pois true é
+//automaticamente convertido para 1 e int2 irá conter o valor 1
+//(false + 1), pois false é convertidp para 0
+
+console.log(a);
+console.log(b);
+console.log(tr);
+console.log(fl);
